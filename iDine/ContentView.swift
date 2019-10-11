32fda5055955102f4ad720093aeeb320068785a9
Erik Krietsch <erik.krietsch@gmail.com>
2019-10-11 13:12:52 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Erik Krietsch on 10/11/19.
+//  Copyright © 2019 Totally Radical Software, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
